3e559ed12a31e1d7fea3f34036fddead5c22c754
ANameSpace <125720576+ANameSpace@users.noreply.github.com>
2025-03-26 23:09:46 +0300

    Add error toast

--- app/src/main/java/com/github/anamespace/todaylist/ui/screens/CreateTaskScreen.kt
+++ app/src/main/java/com/github/anamespace/todaylist/ui/screens/CreateTaskScreen.kt
@@ -20,17 +20,12 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Snackbar
-import androidx.compose.material3.SnackbarDuration
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -42,7 +37,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.anamespace.todaylist.R
 import com.github.anamespace.todaylist.ui.theme.AppTheme
-import kotlinx.coroutines.launch
 import java.time.LocalDate
 import java.time.LocalTime
 import java.util.Calendar
